c55498ceb91fad9582a964825285e0a67a2badb6
asheeshk210 <78215232+asheeshk210@users.noreply.github.com>
2021-05-03 12:55:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function simpleInterest(){
+    var p=document.getElementById('s_amount');
+    var r=document.getElementById('s_rate');
+    var t=document.getElementById('s_time');
+    var si;
+    si=(p*r*t)/100;
+    console.log("Hello world!");
+}
